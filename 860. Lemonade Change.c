9a703f4edb28a4539ecb7c872dc963c982876be1
Jiwon Choi <jwchoi.py@gmail.com>
2020-06-17 19:09:44 +0900

    LeetCode #860

---
+++ 860. Lemonade Change.c
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/lemonade-change/
+ */
+
+bool lemonadeChange(int* bills, int billsSize){
+    int D5 = 0, D10 = 0;
+    bool result = true;
+    
+    for(int i=0; i<billsSize; i++){
+        if(bills[i] == 5) D5++;
+        else if(bills[i] == 10){
+            if(D5 == 0){
+                result = false;
+                break;
+            }else{
+                D5--; D10++;
+            }
+        }
+        else{
+            if(D5>0 && D10>0){
+                D5--; D10--;
+            }
+            else if(D5>=3 && D10==0) D5 -= 3;
+            else{
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
+}
