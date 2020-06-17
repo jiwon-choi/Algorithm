acd9ee488525d73a8be0f3293f4bd1b88a196a3d
Jiwon Choi <jwchoi.py@gmail.com>
2020-06-17 19:09:34 +0900

    LeetCode #771

---
+++ 771. Jewels and Stones.c
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.com/problems/jewels-and-stones/
+ */
+
+int numJewelsInStones(char * J, char * S){
+    int i=0, output=0;
+    while(S[i] != NULL){
+        for(int j=0; J[j]!=NULL; j++)
+            if(S[i] == J[j]) output++;
+    i++;
+    }
+    return output;
+}
