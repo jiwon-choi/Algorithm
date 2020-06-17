62f528eec1e3c54e240251de13e80c69ce7981d1
Jiwon Choi <jwchoi.py@gmail.com>
2020-06-17 19:09:38 +0900

    LeetCode #807

---
+++ 807. Max Increase to Keep City Skyline.c
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+ */
+
+int maxIncreaseKeepingSkyline(int** grid, int gridSize, int* gridColSize){
+    int output = 0, max = 0;
+    int TB[gridSize];
+    int LR[gridSize];
+    
+    for(int i=0; i<gridSize; i++){
+        for(int j=0; j<gridSize; j++){
+            if(max < grid[i][j]) max = grid[i][j];
+        }
+        LR[i] = max;
+        max = 0;
+    }
+    
+    for(int j=0; j<gridSize; j++){
+        for(int i=0; i<gridSize; i++){
+            if(max < grid[i][j]) max = grid[i][j];
+        }
+        TB[j] = max;
+        max = 0;
+    }
+    
+    for(int i=0; i<gridSize; i++){
+        for(int j=0; j<gridSize; j++){
+            int tmp = (LR[i]<TB[j]?LR[i]:TB[j]);
+            if(grid[i][j] < tmp) output += (tmp-grid[i][j]);
+        }
+    }
+    
+    return output;
+}
