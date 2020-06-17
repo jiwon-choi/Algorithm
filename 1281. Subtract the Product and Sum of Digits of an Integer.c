83ec31dc580a357ddc4767aaff760ddb646c6642
Jiwon Choi <jwchoi.py@gmail.com>
2020-06-17 19:09:03 +0900

    LeetCode #1281

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.c
@@ -0,0 +1,13 @@
+/*
+ * https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+ */
+
+int subtractProductAndSum(int n){
+    int product = 1, sum = 0;
+    while(n>0){
+        product *= n%10;
+        sum += n%10;
+        n /= 10;
+    }
+    return product-sum;
+}
