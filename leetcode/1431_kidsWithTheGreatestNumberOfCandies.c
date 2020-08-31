dd57e6fc50e444e00d4e191db7f83a24d4111e12
Jiwon Choi <jwchoi.py@gmail.com>
2020-08-31 18:53:29 +0900

    2020-08-31 18:53

---
+++ leetcode/1431_kidsWithTheGreatestNumberOfCandies.c
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+ */
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize){
+    *returnSize = candiesSize;
+    bool* arr = (bool*)malloc(sizeof(bool)*candiesSize);
+    
+    int maxCandies = 0;
+    for(int i=0; i<candiesSize; i++)
+        maxCandies = candies[i] > maxCandies ? candies[i] : maxCandies;
+    for(int i=0; i<candiesSize; i++){
+        if(candies[i] + extraCandies >= maxCandies) arr[i] = true;
+        else arr[i] = false;
+    }
+    
+    return arr;
+}
