4dc3d333bb6e8eecdb760e68cefbf7249d1ace8d
Jiwon-Choi <jwchoi.py@gmail.com>
2022-01-03 17:31:56 +0900

    baekjoon 10808

---
+++ baekjoon/10808_알파벳 개수.cpp
@@ -0,0 +1,19 @@
+/**
+ * https://www.acmicpc.net/problem/10808
+ */
+
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string input;
+    int cnt[26] = {};
+
+    std::cin >> input;
+    for (int i = 0; i < input.length(); i++)
+        cnt[input[i] - 'a']++;
+    for (int i = 0; i < 26; i++)
+        std::cout << cnt[i] << ' ';
+
+    return (0);
+}
